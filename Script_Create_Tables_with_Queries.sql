47c2879b3fc286d8d3a2af6f36bbb0740d4ae027
pogoryelov <pogoryelov@gmail.com>
2019-10-17 10:15:04 +0200

    Add files via upload
    Added new test SQL queries

--- Script_Create_Tables_with_Queries.sql
+++ Script_Create_Tables_with_Queries.sql
@@ -43,6 +43,12 @@ CREATE TABLE LOCATIONS (
                             PRIMARY KEY (LOCT_ID,DEP_ID_LOC));
 
 
+--------------------------------------------------------
+--------------------------------------------------------
+--LAB: String Patterns, Sorting & Grouping
+--------------------------------------------------------
+--------------------------------------------------------
+
 ---------------------------------------------------
 --Query 1: Retrieve all employees whose address is in Elgin,IL
 ---------------------------------------------------
@@ -124,3 +130,72 @@ select D.dep_name, E.F_name, E.L_name
 from employees as E, departments as D
 where E.dep_ID = D.dept_ID_dep
 order by D.dep_name, E.L_name desc;
+
+SELECT F_NAME, DEP_NAME FROM EMPLOYEES, DEPARTMENTS WHERE DEPT_ID_DEP = DEP_ID;
+
+
+--------------------------------------------------------
+--------------------------------------------------------
+--LAB: JOIN Operations
+--------------------------------------------------------
+--------------------------------------------------------
+
+---------------------------------------------------
+--Query 1A: Select the names and job start dates of all employees who work for the department number 5.
+---------------------------------------------------
+select E.F_name, E.L_name, JH.start_date
+from Employees E inner join Job_history JH
+on E.emp_ID = JH.empl_ID
+where E.dep_ID=5;
+
+---------------------------------------------------
+--Query 1B: Select the names, job start dates, and job titles of all employees who work for the department number 5
+---------------------------------------------------
+select E.F_name, E.L_name, JH.start_date, j.Job_title
+from Employees E
+	inner join Job_history JH on E.emp_ID = JH.empl_ID
+	inner join Jobs J on j.Job_ident = E.Job_ID
+where E.dep_ID=5;
+
+---------------------------------------------------
+--Query 2A: Perform a Left Outer Join on the EMPLOYEES and DEPARTMENT tables and select employee id, last name, department id and department name for all employees
+---------------------------------------------------
+select E.Emp_ID, E.L_name, E.Dep_ID, D.Dep_name
+from Employees E
+left outer join Departments D
+on E.Dep_ID = D.Dept_ID_dep;
+
+---------------------------------------------------
+--Query 2B: Re-write the query for 2A to limit the result set to include only the rows for employees born before 1980
+---------------------------------------------------
+select E.Emp_ID, E.L_name, E.Dep_ID, D.Dep_name
+from Employees E
+left outer join Departments D
+on E.Dep_ID = D.Dept_ID_dep
+where year(E.b_date) < 1980;
+
+---------------------------------------------------
+--Query 2C: Re-write the query for 2A to have the result set include all the employees but department names for only the employees who were born before 1980.
+---------------------------------------------------
+select E.Emp_ID, E.L_name, E.Dep_ID, D.Dep_name
+from Employees E
+left outer join Departments D
+on E.Dep_ID = D.Dept_ID_dep
+and year(E.b_date) < 1980;
+
+---------------------------------------------------
+--Query 3A: Perform a Full Join on the EMPLOYEES and DEPARTMENT tables and select the First name, Last name and Department name of all employees.
+---------------------------------------------------
+select E.F_name, E.L_name, D.Dep_name
+from Employees E
+full outer join Departments D
+on E.Dep_ID = D.Dept_ID_dep;
+
+---------------------------------------------------
+--Query 3B: Re-write Query 3A to have the result set include all employee names but department id and department names only for male employees.
+---------------------------------------------------
+select E.F_name, E.L_name, D.Dept_ID_dep, D.Dep_name
+from Employees E
+full outer join Departments D
+on E.Dep_ID = D.Dept_ID_dep
+and E.Sex = 'M';
